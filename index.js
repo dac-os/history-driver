7c4540490483e2d17296556ec233f78fbc897b7b
Lamasu <rafaerthal@gmail.com>
2014-08-25 23:23:34 -0300

    basic driver communicaition

--- index.js
+++ index.js
@@ -15,3 +15,79 @@ exports.configUri = function (newUri) {
   
   uri = newUri;
 };
+
+exports.histories = function (user, page, next) {
+  'use strict';
+
+  return httpRequest(uri + '/users/' + user + '/histories?page=' + page, function (error, res, body) {
+    var histories;
+    if (error) {
+      error = new VError(error, 'error requesting user "$s" histories', user);
+      return next(error);
+    }
+    try {
+      histories = JSON.parse(body);
+      return next(null, histories);
+    } catch (error) {
+      error = new VError(error, 'error parsing user "$s" histories to json', user);
+      return next(error);
+    }
+  });
+};
+
+exports.history = function (user, id, next) {
+  'use strict';
+
+  return httpRequest(uri + '/users/' + user + '/histories/' + id, function (error, res, body) {
+    var history;
+    if (error) {
+      error = new VError(error, 'error requesting history "$s"', id);
+      return next(error);
+    }
+    try {
+      history = JSON.parse(body);
+      return next(null, history);
+    } catch (error) {
+      error = new VError(error, 'error parsing history "$s" to json', id);
+      return next(error);
+    }
+  });
+};
+
+exports.disciplines = function (user, history, page, next) {
+  'use strict';
+
+  return httpRequest(uri + '/users/' + user + '/histories/' + history + '/disciplines?page=' + page, function (error, res, body) {
+    var disciplines;
+    if (error) {
+      error = new VError(error, 'error requesting user "$s" disciplines', user);
+      return next(error);
+    }
+    try {
+      disciplines = JSON.parse(body);
+      return next(null, disciplines);
+    } catch (error) {
+      error = new VError(error, 'error parsing user "$s" disciplines to json', user);
+      return next(error);
+    }
+  });
+};
+
+exports.discipline = function (user, history, id, next) {
+  'use strict';
+
+  return httpRequest(uri + '/users/' + user + '/histories/' + history + '/disciplines/' + id, function (error, res, body) {
+    var discipline;
+    if (error) {
+      error = new VError(error, 'error requesting discipline "$s"', id);
+      return next(error);
+    }
+    try {
+      discipline = JSON.parse(body);
+      return next(null, discipline);
+    } catch (error) {
+      error = new VError(error, 'error parsing discipline "$s" to json', id);
+      return next(error);
+    }
+  });
+};
