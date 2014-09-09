d94a7f06309065628a9cb8f39c49e3ef834df030
Lamasu <rafaerthal@gmail.com>
2014-09-09 17:43:08 -0300

    new driver structure

--- index.js
+++ index.js
@@ -1,93 +1,72 @@
-var VError, request, async, uri;
-
+var VError, httpRequest, async, uri;
 VError = require('verror');
-request = require('request');
+httpRequest = require('request');
 async = require('async');
 
-/**
- * @method
- * @summary Setups default base uri
- *
- * @param {String} newUri
- */
 exports.configUri = function (newUri) {
   'use strict';
-  
+
   uri = newUri;
 };
 
-exports.histories = function (user, page, next) {
+function downloadPages(url, next) {
   'use strict';
 
-  return httpRequest(uri + '/users/' + user + '/histories?page=' + page, function (error, res, body) {
-    var histories;
-    if (error) {
-      error = new VError(error, 'error requesting user "$s" histories', user);
-      return next(error);
-    }
-    try {
-      histories = JSON.parse(body);
-      return next(null, histories);
-    } catch (error) {
-      error = new VError(error, 'error parsing user "$s" histories to json', user);
-      return next(error);
-    }
+  var lastPage, results, lastLength;
+  results = [];
+  lastLength = 0;
+  lastPage = 0;
+  async.doWhilst(function (next) {
+    return httpRequest({
+      'url'  : uri + url,
+      'json' : true,
+      'qs'   : {'page' : lastPage}
+    }, function (error, res, body) {
+      if (error) {
+        return next(error);
+      }
+      results = results.concat(body);
+      lastLength = body.length;
+      lastPage++;
+      return next();
+    });
+  }, function () {
+    return lastLength > 0;
+  }, function (error) {
+    return next(error, results);
   });
+}
+
+exports.histories = function (user, next) {
+  'use strict';
+
+  return downloadPages('/users/' + user + '/histories', next);
 };
 
 exports.history = function (user, id, next) {
   'use strict';
 
-  return httpRequest(uri + '/users/' + user + '/histories/' + id, function (error, res, body) {
-    var history;
-    if (error) {
-      error = new VError(error, 'error requesting history "$s"', id);
-      return next(error);
-    }
-    try {
-      history = JSON.parse(body);
-      return next(null, history);
-    } catch (error) {
-      error = new VError(error, 'error parsing history "$s" to json', id);
-      return next(error);
-    }
+  return httpRequest({
+    'url'  : uri + '/users/' + user + '/histories/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
   });
 };
 
-exports.disciplines = function (user, history, page, next) {
+exports.disciplines = function (user, history, next) {
   'use strict';
 
-  return httpRequest(uri + '/users/' + user + '/histories/' + history + '/disciplines?page=' + page, function (error, res, body) {
-    var disciplines;
-    if (error) {
-      error = new VError(error, 'error requesting user "$s" disciplines', user);
-      return next(error);
-    }
-    try {
-      disciplines = JSON.parse(body);
-      return next(null, disciplines);
-    } catch (error) {
-      error = new VError(error, 'error parsing user "$s" disciplines to json', user);
-      return next(error);
-    }
-  });
+  return downloadPages('/users/' + user + '/histories/' + history + '/disciplines', next);
 };
 
-exports.discipline = function (user, history, id, next) {
+exports.history = function (user, history, id, next) {
   'use strict';
 
-  return httpRequest(uri + '/users/' + user + '/histories/' + history + '/disciplines/' + id, function (error, res, body) {
-    var discipline;
-    if (error) {
-      error = new VError(error, 'error requesting discipline "$s"', id);
-      return next(error);
-    }
-    try {
-      discipline = JSON.parse(body);
-      return next(null, discipline);
-    } catch (error) {
-      error = new VError(error, 'error parsing discipline "$s" to json', id);
-      return next(error);
-    }
+  return httpRequest({
+    'url'  : uri + '/users/' + user + '/histories/' + history + '/disciplines/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
   });
 };
